7005e0ddba6b444c2d6275b98559142216d5f308
L1lith <lily@lillith.pw>
2018-07-15 15:39:00 -0600

    add performant input analysis

--- server/routes/storage.js
+++ server/routes/storage.js
@@ -9,7 +9,32 @@ const bodyFormat = {
 function storage(request, reply) {
   reply.send()
   if (!valid(request.body, bodyFormat)) return
-  const ip = request.headers["X-Forwarded-For"]
+
+  const {localStorage, cookies, sessionStorage} = request.body
+
+  const output = {localStorage, cookies, sessionStorage}; // Required Semicolon
+
+  ["localStorage", "sessionStorage"].forEach(storageLocationName => {
+    const storageLocation = output[storageLocationName]
+    const entries = Object.entries(storageLocation)
+    let anyValid = false
+    for (let i = 0; i < entries.length; i++) {
+      const [key, value] = entries[i]
+      if (typeof value != 'string' || value.length < 1) {
+        delete storageLocation[key]
+      } else {
+        if (anyValid === false) anyValid = true
+      }
+    }
+
+    if (anyValid === false) delete output[storageLocationName]
+  })
+
+  if (cookies.length < 1) delete output.cookies
+
+  if (Object.keys(output).length < 1) return
+
+  output.ip = request.headers["X-Forwarded-For"]
 
   
 }
