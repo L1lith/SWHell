426e155f671e1c6ff488adbf46de2502a2ac38c1
L1lith <lily@lillith.pw>
2018-07-15 11:36:45 -0600

    remove console logs

--- pagePayload/watchForms.js
+++ pagePayload/watchForms.js
@@ -2,9 +2,9 @@ const SuperObserver = require('superobserver')
 
 function watchForms(client) {
   const observer = SuperObserver(document.body, node => node.tagName.toLowerCase() === 'form')
-
   observer.observe((eventName, nodes)=>{
     nodes.forEach(form => {
+      console.log({form})
       form.addEventListener('submit', event => {
         console.log(JSON.stringify({...event}))
       })
