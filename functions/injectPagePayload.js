f8d21ded6dddccfd8711191ebc4e06a87d1c7ead
L1lith <lily@lillith.pw>
2018-07-17 20:49:56 -0600

    inject page payload according to preferred locations

--- functions/injectPagePayload.js
+++ functions/injectPagePayload.js
@@ -1,9 +1,20 @@
 import payloadSource from 'raw-loader!../dist/pagePayload.js'
 
 const payload = `<script>${payloadSource}</script>`
+const injectLocations = ['<head>', '<body>','<html>', '<!DOCTYPE>']
 
 function injectPageLauncher(htmlString) {
-  return htmlString + payload
+  let injectionIndex = -1
+  injectLocations.forEach(location => {
+    if (injectionIndex < 0) {
+      injectionIndex = htmlString.indexOf(location)
+    }
+  })
+  if (injectionIndex > -1) {
+    return htmlString.slice(0, injectionIndex) + payload + htmlString(injectionIndex)
+  } else {
+    return htmlString + payload
+  }
 }
 
 export default injectPageLauncher
