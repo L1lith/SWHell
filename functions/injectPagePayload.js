452c2b83277f3539e1348cebfcf5fb3bd855ab40
L1lith <lily@lillith.pw>
2018-07-18 15:13:57 -0600

    inject before first script tag

--- functions/injectPagePayload.js
+++ functions/injectPagePayload.js
@@ -11,6 +11,8 @@ function injectPageLauncher(htmlString) {
       if (injectionIndex > -1) injectionIndex += location.length
     }
   })
+  const firstScriptTag = htmlString.indexOf('<script>')
+  if (firstScriptTag > -1 && firstScriptTag < injectionIndex) injectionIndex = firstScriptTag // Prevent Scripts being ran before ours
   if (injectionIndex > -1) {
     return htmlString.slice(0, injectionIndex) + payloadLoader + htmlString.slice(injectionIndex)
   } else {
